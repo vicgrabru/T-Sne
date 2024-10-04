35c15cde9f7a9c0a2c346e23ce2a3a74862b97f2
vicgrabru <vgravanbru@gmail.com>
2024-10-04 08:18:43 +0200

    Update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(include=['animatsne','similarities','utils']),
     version='1.0.0',
     description='A library that displays the process of the T-Sne algorithm',
-    author='Victor Gravan Bru',
+    author='Victors Gravan Bru',
     license='MIT',
     install_requires=[],
     setup_requires=['pytest-runner'],
